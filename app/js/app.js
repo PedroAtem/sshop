b3e54fef9a9664507ad864094b4a25b6fd9202f6
Pedro Henrique Silva Rosa <pedro.rosa@agrosatelite.com.br>
2019-02-28 19:15:34 -0300

    interface de lista de produtos

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 import '../scss/style.scss';
 import '../scss/header.scss';
 import '../scss/sidenav.scss';
+import '../scss/productList.scss';
+import '../scss/table.scss';
 
 angular.module("sshop", ['ngMaterial', 'ngMessages', 'ngRoute']);
 angular.module("sshop").controller("sshopCtrl", function ($scope) {
--- app/js/controllers/productListCtrl.js
+++ app/js/controllers/productListCtrl.js
@@ -2,5 +2,6 @@ angular.module("sshop").controller("productListCtrl", function($scope, products)
 
     $scope.title = 'sdbfinsdf';
     $scope.products = products.data;
+    $scope.products = $scope.products.sort((a, b) => a.id - b.id);
 
 });
